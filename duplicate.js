6d5f1d755022553c5e4dc2055c3093dc1002ce02
MdMizanurRahman1 <mizan.rahman2217@gmail.com>
2023-02-05 00:14:56 +0200

    remove duplicate practiced

--- duplicate.js
+++ duplicate.js
@@ -1,1 +1,14 @@
 const names = ['habul', 'babul', 'sabul', 'jabul', 'habul', 'cabul', 'tabul', 'modul', 'habul', 'babul', 'sabul'];
+
+function removeDuplicate(names) {
+    const unique = [];
+    for (let i = 0; i < names.length; i++) {
+        const name = names[i];
+        if (unique.includes(name) === false) {
+            unique.push(name);
+        }
+    }
+    return unique;
+}
+const realName = removeDuplicate(names);
+console.log(realName);
